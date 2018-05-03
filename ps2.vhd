22425e3aa24a4ca980bb255015ed63aa3071e080
Unknown <xinlinchen2015@u.northwestern.edu>
2018-05-02 21:25:12 -0500

    ASCII table with capital letters

--- ps2.vhd
+++ ps2.vhd
@@ -42,6 +42,144 @@ signal read : std_logic;
 
 begin
 
+if (history1 = X"12") or (history1 = X"59") then
+	case(key) is
+		when X"0E" =>
+			tempkey <= X"60";
+		when X"16" =>
+			tempkey <= X"31";
+		when X"1E" =>
+			tempkey <= X"32";
+		when X"26" =>
+			tempkey <= X"33";
+		when X"25" =>
+			tempkey <= X"34";
+		when X"2E" =>
+			tempkey <= X"35";
+		when X"36" =>
+			tempkey <= X"36";
+		when X"3D" =>
+			tempkey <= X"37";
+		when X"3E" =>
+			tempkey <= X"38";
+		when X"46" =>
+			tempkey <= X"39";
+		when X"45" =>
+			tempkey <= X"30";
+		when X"4E" =>
+			tempkey <= X"2D";
+		when X"55" =>
+			tempkey <= X"3D";
+		when X"66" =>
+			tempkey <= X"08";
+		when X"0D" =>
+			tempkey <= X"09"; --'horizontal tab'
+		when X"15" => --Q
+			tempkey <= X"51";
+		when X"1D" => --W
+			tempkey <= X"57";
+		when X"24" => --E
+			tempkey <= X"45";
+		when X"2D" => --R
+			tempkey <= X"52";
+		when X"2C" => --T
+			tempkey <= X"54";
+		when X"35" => --Y
+			tempkey <= X"59";
+		when X"3C" => --U
+			tempkey <= X"55";
+		when X"43" => --I
+			tempkey <= X"49";
+		when X"44" => --O
+			tempkey <= X"4F";
+		when X"4D" => --P
+			tempkey <= X"50";
+		when X"54" => --[
+			tempkey <= X"5B";
+		when X"5B" => --]
+			tempkey <= X"5D";
+		when X"5D" => --\
+			tempkey <= X"5C";
+		when X"58" => --Caps lock
+			tempkey <= X"80";
+		when X"1C" => --A
+			tempkey <= X"41";
+		when X"1B" => --S
+			tempkey <= X"53";
+		when X"23" => --D
+			tempkey <= X"44";
+		when X"2B" => --F
+			tempkey <= X"46";
+		when X"34" => --G
+			tempkey <= X"47";
+		when X"33" => --H
+			tempkey <= X"48";
+		when X"3B" => --J
+			tempkey <= X"4A";
+		when X"42" => --K
+			tempkey <= X"4B";
+		when X"4B" => --L
+			tempkey <= X"4C";
+		when X"4C" => --;
+			tempkey <= X"3B";
+		when X"52" => --'
+			tempkey <= X"27";
+		when X"5A" => --Enter
+			tempkey <= X"81";
+		when X"12" => --Left shift
+			tempkey <= X"82";
+		when X"1A" => --Z
+			tempkey <= X"5A";
+		when X"22" => --X
+			tempkey <= X"58";
+		when X"21" => --C
+			tempkey <= X"43";
+		when X"2A" => --V
+			tempkey <= X"56";
+		when X"32" => --B
+			tempkey <= X"42";
+		when X"31" => --N
+			tempkey <= X"4E";
+		when X"3A" => --M
+			tempkey <= X"4D";
+		when X"41" => --,
+			tempkey <= X"41";
+		when X"49" => --.
+			tempkey <= X"49";
+		when X"4A" => --/
+			tempkey <= X"4A";
+		when X"59" => --Right shift
+			tempkey <= X"83";
+		when X"14" => --Left ctl
+			tempkey <= X"84";
+		when X"11" => --Left alt
+			tempkey <= X"85";
+		when X"29" => --Space bar
+			tempkey <= X"20";
+		when X"70" => --0
+			tempkey <= X"30";
+		when X"71" => --.
+			tempkey <= X"2E";
+		when X"69" => --1
+			tempkey <= X"31";
+		when X"72" => --2
+			tempkey <= X"32";
+		when X"7A" => --3
+			tempkey <= X"33";
+		when X"6B" => --4
+			tempkey <= X"34";
+		when X"73" => --5
+			tempkey <= X"35";
+		when X"74" => --6
+			tempkey <= X"36";
+		when X"6C" => --7
+			tempkey <= X"37";
+		when X"75" => --8
+			tempkey <= X"38";
+		when X"7D" => --9
+			tempkey <= X"39";
+	end case;
+else
 	case(key) is
 		when X"0E" =>
 			tempkey <= X"60";
@@ -177,9 +315,8 @@ begin
 			tempkey <= X"38";
 		when X"7D" => --9
 			tempkey <= X"39";
-		
 	end case;
-		
+end if;
 
 u1: keyboard port map(	
 				keyboard_clk => keyboard_clk,
